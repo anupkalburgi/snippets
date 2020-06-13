c6822cca515c9277a40971a935f577aa1c50f584
anupkalburgi <anupkalburgi@gmail.com>
2020-06-13 17:42:05 -0400

    May be simple way of adding number, have to write it python and check

--- scala/snippet/src/main/scala/BinaryAdd.scala
+++ scala/snippet/src/main/scala/BinaryAdd.scala
@@ -2,4 +2,41 @@ package snippet
 
 object BinaryAdd extends App {
   println("So that is the diff")
+  val ll = List(1,0,0,1,1)
+  val rr = List(1,1,1,0,1)
+  println(addBinary(ll.reverse, rr.reverse, 0, List.empty[Int]))
+  // Find out how zipAll works
+
+  def addBinary(
+      l: List[Int],
+      r: List[Int],
+      carry: Int,
+      ans: List[Int]
+  ): List[Int] = {
+    (l, r, carry) match {
+      case (Nil, Nil , _) => carry :: ans
+      case (lh :: lt, Nil, c) => l ++ ans // I have to add c to lh
+      case (Nil, rh :: rt, c) => r ++ ans // I have to add c to lh
+      case (lh :: lt, rh :: rt, c) =>
+        val (c1, n1) = addBin(lh, rh, c)
+        addBinary(lt, rt, c1, n1 :: ans)
+      // case _ => throw IllegalArgumentException("not this please")
+    }
+  }
+
+  def addBin(a: Int, b: Int, carry: Int): (Int, Int) = {
+    (a, b, carry) match {
+      case (0, 0, 0) => (0, 0)
+      case (0, 0, 1) => (0, 1)
+      case (0, 1, 0) => (0, 1)
+      case (0, 1, 1) => (1, 0)
+      case (1, 0, 0) => (0, 1)
+      case (1, 0, 1) => (1, 0)
+      case (1, 1, 0) => (1, 0)
+      case (1, 1, 1) => (1, 1)
+      // case _ => new IllegalArgumentException
+    }
+    // }
+
+  }
 }
--- scala/snippet/test/src/scala/BinaryAddSpec.scala
+++ scala/snippet/test/src/scala/BinaryAddSpec.scala
@@ -5,8 +5,11 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class HelloSuite extends AnyFunSuite {
 
-    test("Test that ‘Hello’ string is correct") {
+    test("Binrary adding, also needs an algorithms") {
         assert("Hello, world" == "Hello, world")
+        val ll = List(1,0,0,1,1)
+        val rr = List(1,1,1,0,1)
+        assert (BinaryAdd.addBinary(ll.reverse, rr.reverse, 0, List.empty[Int]) == List(1, 1, 0, 0, 0, 0))
     }
 
     test ("Another test ...") (pending)
