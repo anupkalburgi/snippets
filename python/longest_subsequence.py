f85cac70d48cce61fba0c6c597c8c4077b7e1ec7
anupkalburgi <anupkalburgi@gmail.com>
2020-12-31 06:46:50 -0500

    adding a daily commit now on so that i can keep changing the code

--- python/longest_subsequence.py
+++ python/longest_subsequence.py
@@ -51,12 +51,13 @@ def lcs(s1, s2, id1, id2, size):
     """
     if id1 == len(s1) or id2 == len(s2):
         return size
+    # c1 = size
     if s1[id1] == s2[id2]:
-        size = lcs(s1, s2, id1+1, id2+1, size+1)
-    c1 = lcs(s1, s2, id1+1, id2, 0)
-    c2 = lcs(s1 , s2, id1, id2+1, 0)
+        return lcs(s1, s2, id1+1, id2+1, size+1)
+    c2 = lcs(s1, s2, id1+1, id2, 0)
+    c3 = lcs(s1 , s2, id1, id2+1, 0)
     
-    return max(size, max(c1, c2))
+    return max(size, c2, c3)
 
 
 def lcs_memo(s1, s2):
@@ -95,7 +96,99 @@ def lcs_dp(s1, s2):
 
 
 
-print(lcs("abdca", "cbda", 0 , 0 , 0))
+print("abdca", "cbda" , "expect 2 ->", lcs("abdca", "cbda", 0 , 0 , 0)) # 2
+print("passport", "ppsspt", "expect 3 ->" , lcs("passport", "ppsspt", 0 ,0 ,0)) # 3
 print(lcs_memo("abdca", "cbda"))
 print(lcs_dp("abdcfa", "cbda"))
 
+print("Substring End ###################################################")
+
+###################################################
+# subsequence, without changing the order, 
+#  deleteing elements from the either
+# TODO: generting substring vs generating subsequence
+###################################################
+
+def longest_subsequence(s1, s2, i, j):
+    if i == len(s1) or j == len(s2):
+        return 0
+    else:
+        if s1[i] == s2[j]:
+            return 1 + longest_subsequence(s1, s2, i+1, j+1)
+        c1 = longest_subsequence(s1, s2, i+1, j )
+        c2 = longest_subsequence(s1, s2, i, j+1)
+        return max(c1, c2)
+
+
+def longest_subsequence_memo(s1, s2):
+    memo = [[-1 for _ in  range(len(s2))] for _ in range(len(s1))]
+
+    def lsm_rec(s1, s2, i, j):
+        if i == len(s1)  or j == len(s2):
+            return 0
+        else:
+            if memo[i][j] == -1:
+                if s1[i] == s2[j]:
+                    memo[i][j] = 1 + lsm_rec(s1, s2, i+1, j+1)
+                else:
+                    c2 = lsm_rec(s1, s2, i+1, j)
+                    c3 = lsm_rec(s1, s2, i, j+1)
+                    memo[i][j] = max(c2, c3)
+                return memo[i][j]
+            return memo[i][j]
+            
+    return lsm_rec(s1, s2, 0, 0)
+
+def longest_subsequence_dp(s1, s2):
+    dp = [[0 for _ in range(len(s2)+1)] for _ in range(len(s1)+1)]
+    max_len = 0
+    for i in range(1, len(s1)+1):
+        for j in range(1, len(s2)+1):
+            if s1[i-1] == s2[j-1]:
+                dp[i][j] = 1 + dp[i-1][j-1]
+            else:
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+            max_len = max(max_len, dp[i][j])
+    return max_len
+
+
+assert longest_subsequence("abdca", "cbda", 0 , 0) == 3
+assert longest_subsequence("passport", "ppsspt", 0, 0) == 5
+assert longest_subsequence_memo("abdca", "cbda") == 3
+assert longest_subsequence_memo("passport", "ppsspt") == 5
+print("abdca", "cbda" , "expect 3 -> lcs ->>", longest_subsequence("abdca", "cbda", 0 , 0)) 
+print("passport", "ppsspt", "expect 5 -> lcs_memo ->>" , longest_subsequence_memo("passport", "ppsspt"))
+
+print("abdca", "cbda" , "expect 3 -> lcs_dp ->>", longest_subsequence_dp("abdca", "cbda")) 
+print("passport", "ppsspt", "expect 5 -> lcs_dp ->>" , longest_subsequence_dp("passport", "ppsspt"))
+
+print("subsequence End ###################################################")
+###################################################
+# given 2 strings, s1, s2, what are the no. of inserts and deletes needed to transform s1 => s2
+# n1 = len(s1)
+# n2 = len(s2)
+# lcs_len = lcs_dp(s1, s2)
+# number_deletes = n1 - lcs_len 
+# number_inserts = n2 - lcs_len
+###################################################
+
+
+def inserts_deletes_lcs(s1, s2):
+    n1 = len(s1)
+    n2 = len(s2)
+    dp = [[0 for _ in range(n2+1)] for _ in range(n1+1)]
+    lcs_len = 0
+    
+    for i in range(1, n1+1):
+        for j in range(1, n2+1):
+            if s1[i-1] == s2[j-1]:
+                dp[i][j] = 1 + dp[i-1][j-1]
+            else:
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+            lcs_len = max(lcs_len, dp[i][j])
+    
+    return ((n1-lcs_len, n2-lcs_len))
+
+print(inserts_deletes_lcs("abc", "fbc"))
+print(inserts_deletes_lcs("abdca", "cbda"))
+print(inserts_deletes_lcs("passport", "ppsspt"))
